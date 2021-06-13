f60edc81ea1cde012eeb33ccaaf68d7483d66ebd
Motta-Felipe <flp.motta@hotmail.com>
2021-06-13 19:27:04 +0200

    created domain classes

---
+++ src/main/java/com/felipe/bookstore/domain/Book.java
@@ -0,0 +1,87 @@
+package com.felipe.bookstore.domain;
+
+public class Book {
+	private int id;
+	private String title;
+	private String name_author;
+	private String text;
+
+	private Category category;
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book(int id, String title, String name_author, String text, Category category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.name_author = name_author;
+		this.text = text;
+		this.category = category;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName_author() {
+		return name_author;
+	}
+
+	public void setName_author(String name_author) {
+		this.name_author = name_author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/felipe/bookstore/domain/Category.java
@@ -0,0 +1,82 @@
+package com.felipe.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+	private int id;
+	private String name;
+	private String description;
+	
+	private List<Book> books = new ArrayList<>();
+
+	public Category() {
+	}
+
+	public Category(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+
+}
