cf3e0278393520d2c18aba5db9d123223d8066e9
Felipe Motta <flp.motta@hotmail.com>
2021-07-09 11:48:01 +0200

    Personalized Exception for Not found Object

--- src/main/java/com/felipe/bookstore/services/CategoryService.java
+++ src/main/java/com/felipe/bookstore/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.felipe.bookstore.services;
 
 import com.felipe.bookstore.domain.Category;
 import com.felipe.bookstore.repositories.CategoryRepository;
+import com.felipe.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class CategoryService {
 
     public Category findById(Integer id){
         Optional<Category> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() ->new ObjectNotFoundException("Object not Found! Id: " + id + ", Type: " + Category.class.getName()));
     }
 }
